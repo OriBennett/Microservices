f706a99f8eefe2c46e3cb21f2107be009c7ea3f6
Ori <OriBennett@gmail.com>
2024-03-21 14:41:02 +0200

    verification

--- Play.Catalog/src/Play.Catalog.Service/Dtos.cs
+++ Play.Catalog/src/Play.Catalog.Service/Dtos.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,7 @@ namespace Play.Catalog.Service.Dtos
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
 
-    public record CreateItemDto(string Name, string Description, decimal Price);
+    public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 
-    public record UpdateItemDto(string Name, string Description, decimal Price);
-
-
+    public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 }
