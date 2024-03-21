99b85201d2801858f50caf1659fb1673acf10ac1
Ori <OriBennett@gmail.com>
2024-03-21 16:29:42 +0200

    finished for today

--- Play.Catalog/src/Play.Catalog.Service/Repositories/ItemsRepositories.cs
+++ Play.Catalog/src/Play.Catalog.Service/Repositories/ItemsRepositories.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using MongoDB.Driver;
 using Play.Catalog.Service.Entities;
@@ -22,6 +23,29 @@ namespace Play.Catalog.Service.Repositories
         {
             return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
         }
-
+        public async Task<Item> GetAsync(Guid id){
+            FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.Id, id);
+            return await dbCollection.Find(filter).SingleOrDefaultAsync();
+        }
+        public async Task CreateAsync(Item entity){
+            if (entity == null){
+                throw new ArgumentNullException(nameof(entity));
+            }
+            await dbCollection.InsertOneAsync(entity);
+        }
+        public async Task UpdateAsync(Item entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            FilterDefinition<Item> filter = filterBuilder.Eq(existingEntity => existingEntity.Id, entity.Id);
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+        public async Task RemoveAsync(Guid id)
+        {          
+            FilterDefinition<Item> filter = filterBuilder.Eq(entity => entity.Id, id);
+            await dbCollection.DeleteOneAsync(filter);
+        }
     }
 }
